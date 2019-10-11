e0c5c7e03121446af7c97e39f16ce2c84071e410
Einherjarn <31859362+Einherjarn@users.noreply.github.com>
2019-10-11 12:17:10 +0300

    Delete card_hand.py

--- card_hand.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class card_base(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("card_dev_example.png")
-        #self.image.set_colorkey((255,255,255))
-        self.rect = self.image.get_rect()
-        self.basecost = 0
